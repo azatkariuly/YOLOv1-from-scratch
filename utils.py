ba31d37477f690ef61818d886c59f266e8722d6e
Azat Azamat <azat.kariuly@gmail.com>
2025-03-20 22:15:43 +0900

    update cellbox

--- utils.py
+++ utils.py
@@ -287,7 +287,7 @@ def get_bboxes(
 
 
 
-def convert_cellboxes(predictions, S=7):
+def convert_cellboxes(predictions, S=7, C=3):
     """
     Converts bounding boxes output from Yolo with
     an image split size of S into entire image ratios
@@ -300,11 +300,11 @@ def convert_cellboxes(predictions, S=7):
 
     predictions = predictions.to("cpu")
     batch_size = predictions.shape[0]
-    predictions = predictions.reshape(batch_size, 7, 7, 30)
-    bboxes1 = predictions[..., 21:25]
-    bboxes2 = predictions[..., 26:30]
+    predictions = predictions.reshape(batch_size, 7, 7, C + 10)
+    bboxes1 = predictions[..., C + 1:C + 5]
+    bboxes2 = predictions[..., C + 6:C + 10]
     scores = torch.cat(
-        (predictions[..., 20].unsqueeze(0), predictions[..., 25].unsqueeze(0)), dim=0
+        (predictions[..., C].unsqueeze(0), predictions[..., C + 5].unsqueeze(0)), dim=0
     )
     best_box = scores.argmax(0).unsqueeze(-1)
     best_boxes = bboxes1 * (1 - best_box) + best_box * bboxes2
@@ -313,8 +313,8 @@ def convert_cellboxes(predictions, S=7):
     y = 1 / S * (best_boxes[..., 1:2] + cell_indices.permute(0, 2, 1, 3))
     w_y = 1 / S * best_boxes[..., 2:4]
     converted_bboxes = torch.cat((x, y, w_y), dim=-1)
-    predicted_class = predictions[..., :20].argmax(-1).unsqueeze(-1)
-    best_confidence = torch.max(predictions[..., 20], predictions[..., 25]).unsqueeze(
+    predicted_class = predictions[..., :C].argmax(-1).unsqueeze(-1)
+    best_confidence = torch.max(predictions[..., C], predictions[..., C + 5]).unsqueeze(
         -1
     )
     converted_preds = torch.cat(
@@ -322,7 +322,6 @@ def convert_cellboxes(predictions, S=7):
     )
 
     return converted_preds
-
 
 def cellboxes_to_boxes(out, S=3):
     converted_pred = convert_cellboxes(out).reshape(out.shape[0], S * S, -1)
